3cca173a80f17143752bbb88c174444ef9801d8e
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-11-20 18:00:03 +0000

    Changes pictures size

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,11 +1,11 @@
 const MyProjects = () => {
     return (
-        <div>
-            <h1>Check out my projects</h1>
+        <div className=''>
+            <h1 className='text-3xl pl-36 mb-2 mt-24'>Check out my projects</h1>
 
-            <img src='/src/assets/ProjectsPics/Screenshot 2024-09-04 at 13.29.52.png' />
-            <img src='/src/assets/ProjectsPics/Screenshot 2024-09-17 at 18.47.32.png' />
-            <img src='/src/assets/ProjectsPics/Screenshot 2024-09-17 at 18.53.22.png' />
+            <img className='pl-36 w-96 hover:w-[450px]' src='/src/assets/ProjectsPics/Screenshot 2024-09-04 at 13.29.52.png' />
+            <img className='pl-36 w-96 hover:w-[450px]' src='/src/assets/ProjectsPics/Screenshot 2024-09-17 at 18.47.32.png' />
+            <img className='pl-36 w-96 hover:w-[450px]' src='/src/assets/ProjectsPics/Screenshot 2024-09-17 at 18.53.22.png' />
         </div>
     )
 }
