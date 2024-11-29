ea1c24efed43a1ac4adef7a9416b65852ff7c068
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-11-29 15:01:54 +0000

    TuneTracker started in projects page

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -89,6 +89,74 @@ function ProjectsPage() {
                 <p>Back-end: Express, Node.js, MongoDB, Postman</p>
             </div>
         </div>
+        <div>
+            <h1 className='pl-10 pt-12 font-bold text-[28px]'>TuneTracker</h1>
+
+            <p className='pl-10 pt-6 w-3/4'>TuneTracker is a music albums browser. Users can browse recently released albums, rate them, add to &apos;myalbums&apos; &apos;wishlist&apos; folders. The application is fully responsive having both desktop and mobile views. It is Single Page Application (SPA), created using React.js, consisting of multiple views. It is integrated with a mock backend (JSON server) and the Spotify API. The app performs all CRUD operations.</p>
+            <div className='flex mt-14 pl-10 mb-14'>
+                <a
+                	href='https://tunetracker90.netlify.app/'
+					target='_blank'
+                >
+                    <div className='flex flex-col items-center mr-16'>
+                        <Globe 
+                            size={32}
+                        />
+                        <h3>Website</h3>
+                    </div>
+                </a>
+                <a
+                	href='https://github.com/MagdaDra/TuneTracker'
+					target='_blank'
+                >
+                <div className='flex flex-col items-center mr-16'>
+                    <GithubLogo 
+                        size={32}
+                    />
+                    <h3>Github Repository Link</h3>     
+                </div>
+                </a>
+
+            </div>
+            
+            <div>
+                <h3 className='pl-10 text-[14px]'>Spotify API allows only 10 emails to be added to access the free API version. Please use the credentials below to login:</h3>
+                <div className='pt-2'>
+                    <p className='pl-10 text-[14px]'>email: tunetracker90@gmail.com</p>
+                    <p className='pl-10 text-[14px]'>password: tunetracker90</p>
+                </div>
+
+            </div>
+
+            <div>
+                <h2 className='pl-10 pt-12 text-[22px]'>Desktop view</h2>
+                <div className='flex pt-2'>
+                    <div>
+                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/homepage.png' />
+                    </div>
+                    <div>
+                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/main view.png' />
+                    </div>
+                    <div>
+                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/client dashboard.png' />
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <h2 className='pl-10 pt-12 text-[22px]'>Mobile view</h2>
+                <div className='flex pt-2'>    
+                    <img className='w-48 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/resp homepage.png' />
+                    <img className='w-48 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/resp mainpage.png' />
+                    <img className='w-48 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/resp dashboard.png' />
+                </div>
+            </div>
+            <div className='pl-10 pt-6 mb-16'>
+                <h2 className='pb-4 pt-8 text-[22px]'>Technologies and tools</h2>
+                <p className='mb-2 pt-2'>Front-end: React, Tailwind</p>
+                <p>Back-end: Express, Node.js, MongoDB, Postman</p>
+            </div>
+        </div>
 
 
         <img className='w-96' src='/src/assets/ProjectsPics/Screenshot 2024-09-17 at 18.47.32.png' />
