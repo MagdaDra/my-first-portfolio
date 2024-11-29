3582c8655e8a7562f144fb2fbb8678c3d1cebf24
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-11-29 16:35:27 +0000

    Projects Page finished

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -4,7 +4,7 @@ function ProjectsPage() {
   return (
     <div>
         <div>
-            <h1 className='pl-10 pt-36 font-bold text-[28px]'>oMERCADO</h1>
+            <h1 className='pl-10 pt-24 font-bold text-[28px]'>oMERCADO</h1>
 
             <p className='pl-10 pt-6 w-3/4'>oMERCADO is an e-commerce store for services with a customer and seller view. The website has authentication feature where user can choose whether to have &apos;customer&apos;, &apos;seller&apos; profile or both. Sellers can add their services to sell and control their stock. Customers can purchase services through the cart function and view their past transactions.</p>
             <div className='flex mt-14 pl-10 mb-14'>
@@ -132,13 +132,13 @@ function ProjectsPage() {
                 <h2 className='pl-10 pt-12 text-[22px]'>Desktop view</h2>
                 <div className='flex pt-2'>
                     <div>
-                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/homepage.png' />
+                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/TuneTracker/tt main page.png' />
                     </div>
                     <div>
-                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/main view.png' />
+                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/TuneTracker/tt album.png' />
                     </div>
                     <div>
-                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/client dashboard.png' />
+                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/TuneTracker/tt my albums.png' />
                     </div>
                 </div>
             </div>
@@ -146,21 +146,70 @@ function ProjectsPage() {
             <div>
                 <h2 className='pl-10 pt-12 text-[22px]'>Mobile view</h2>
                 <div className='flex pt-2'>    
-                    <img className='w-48 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/resp homepage.png' />
-                    <img className='w-48 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/resp mainpage.png' />
-                    <img className='w-48 pl-10 pt-6' src='/src/assets/ProjectsPics/oMERCADO/resp dashboard.png' />
+                    <img className='w-48 pl-10 pt-6' src='/src/assets/ProjectsPics/TuneTracker/responsive main.png' />
+                    <img className='w-48 pl-10 pt-6' src='/src/assets/ProjectsPics/TuneTracker/responsive album.png' />
+                    <img className='w-48 pl-10 pt-6' src='/src/assets/ProjectsPics/TuneTracker/responsive my albums.png' />
                 </div>
             </div>
             <div className='pl-10 pt-6 mb-16'>
                 <h2 className='pb-4 pt-8 text-[22px]'>Technologies and tools</h2>
-                <p className='mb-2 pt-2'>Front-end: React, Tailwind</p>
-                <p>Back-end: Express, Node.js, MongoDB, Postman</p>
+                <p className='mb-2 pt-2'>Front-end: React, CSS, Chakra UI</p>
+                <p>Back-end: mock JSON server</p>
+            </div>
+        </div>
+        <div>
+            <h1 className='pl-10 pt-12 font-bold text-[28px]'>Portuguese Food Ninja</h1>
+
+            <p className='pl-10 pt-6 w-3/4'>PortugueseFoodNinja is a clicking game, built with JavaScript, HTML, and CSS. The player has to click all the food appearing on the screen in order to earn points. Once a bomb appears, a player has 5 seconds to click it. Otherwise it explodes and the game ends. If a player clicks a rooster, they gain one life. The higher the score the faster objects move. </p>
+            <div className='flex mt-14 pl-10 mb-14'>
+                <a
+                	href='https://pt-foodninja.netlify.app/'
+					target='_blank'
+                >
+                    <div className='flex flex-col items-center mr-16'>
+                        <Globe 
+                            size={32}
+                        />
+                        <h3>Website</h3>
+                    </div>
+                </a>
+                <a
+                	href='https://github.com/MagdaDra/PortugueseFoodNinja'
+					target='_blank'
+                >
+                <div className='flex flex-col items-center mr-16'>
+                    <GithubLogo 
+                        size={32}
+                    />
+                    <h3>Github Repository Link</h3>     
+                </div>
+                </a>
+
+            </div>
+            
+
+            <div>
+                <h2 className='pl-10 pt-12 text-[22px]'>Desktop view</h2>
+                <div className='flex pt-2'>
+                    <div>
+                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/Pt Food Ninja/fn main.png' />
+                    </div>
+                    <div>
+                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/Pt Food Ninja/fn game.png' />
+                    </div>
+                    <div>
+                    <img className='w-96 pl-10 pt-6' src='/src/assets/ProjectsPics/Pt Food Ninja/fn game over.png' />
+                    </div>
+                </div>
+            </div>
+
+            <div className='pl-10 pt-6 mb-16'>
+                <h2 className='pb-4 pt-8 text-[22px]'>Technologies and tools</h2>
+                <p className='mb-2 pt-2'>JavaScript, HTML, CSS, DOM Manipulation</p>
             </div>
         </div>
 
 
-        <img className='w-96' src='/src/assets/ProjectsPics/Screenshot 2024-09-17 at 18.47.32.png' />
-        <img className='w-96' src='/src/assets/ProjectsPics/Screenshot 2024-09-17 at 18.53.22.png' />
     </div>
   )
 }
