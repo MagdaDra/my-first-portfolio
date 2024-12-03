ec337b184a9fc801cfa5716bf19b866d1cc3a8fc
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-12-03 15:26:38 +0000

    /public removed from the pics path

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -6,9 +6,9 @@ const MyProjects = () => {
             <h1 className='text-3xl pl-36 mt-24'>Check out my projects</h1>
             <div className='flex-col justify-items-end pr-56'>
             
-                <img className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff]  w-96 hover:w-[450px] mb-10 mt-8' src='/public/assets/ProjectsPics/oMERCADO/homepage.png' />
-                <img className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff]  w-96 hover:w-[450px] mb-10 mr-48' src='/public/assets/ProjectsPics/Screenshot 2024-09-17 at 18.47.32.png' />
-                <img className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff]  w-96 hover:w-[450px] mb-8 mr-12' src='/public/assets/ProjectsPics/Screenshot 2024-09-17 at 18.53.22.png' />
+                <img className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff]  w-96 hover:w-[450px] mb-10 mt-8' src='/assets/ProjectsPics/oMERCADO/homepage.png' />
+                <img className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff]  w-96 hover:w-[450px] mb-10 mr-48' src='/assets/ProjectsPics/Screenshot 2024-09-17 at 18.47.32.png' />
+                <img className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff]  w-96 hover:w-[450px] mb-8 mr-12' src='/assets/ProjectsPics/Screenshot 2024-09-17 at 18.53.22.png' />
             </div>
         </div>
     )
