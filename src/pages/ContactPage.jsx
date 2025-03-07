6b62502d34b956235703cd82e5387be9d4c57070
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2025-03-07 18:46:44 +0100

    Contact Page responsiveness fixed

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -7,19 +7,19 @@ function ContactPage() {
 	return (
 		<div className='flex flex-col min-h-screen'>
 			<div className='flex-grow'>
-				<h1 className='text-3xl pl-36 mb-2 pt-24'>Contact</h1>
-				<p className='pl-36 text-xl mb-2 pt-4'>Magdalena Drąszczyk</p>
-				<div className='flex pl-[141px] mt-4'>
-					<LinkedinLogo size={32} />
-					<span className='pl-2 mt-1 hover:underline hover:text-[#7b12ff]'>
+				<h1 className='text-3xl pl-8 md:pl-36 mb-2 pt-24'>Contact</h1>
+				<p className='pl-8 md:pl-36 text-xl mb-2 pt-4'>Magdalena Drąszczyk</p>
+				<div className='flex pl-8 text-[10px] md:text-sm md:pl-[141px] mt-4'>
+					<LinkedinLogo size={16}/>
+					<span className='pl-2 hover:underline hover:text-[#7b12ff]'>
 						<NavLink to='https://www.linkedin.com/in/magdalenadraszczyk/'>
 							https://www.linkedin.com/in/magdalenadraszczyk/
 						</NavLink>
 					</span>
 				</div>
-				<div className='flex pl-[141px] mt-4'>
-					<GithubLogo size={32} />
-					<span className='pl-2 mt-1 hover:underline hover:text-[#7b12ff]'>
+				<div className='flex text-[10px] md:text-sm pl-8 md:pl-[141px] mt-4'>
+					<GithubLogo size={16} />
+					<span className='pl-2 hover:underline hover:text-[#7b12ff]'>
 						<NavLink to='https://github.com/MagdaDra'>
 							https://github.com/MagdaDra
 						</NavLink>
