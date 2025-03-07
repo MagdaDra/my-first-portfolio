2937daa6e10ebe1758cff0183bcafd6459200291
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2025-03-07 17:29:19 +0100

    AboutMe descripiton updated

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,11 +3,13 @@ const AboutMe = () => {
 		<div>
             <h1 className="text-3xl pl-8 md:pl-36 mb-2">About me</h1>
 			<p className='pl-8 md:pl-36 md:text-justify w-4/5 leading-7'>
-				I am a Full Stack Developer with a problem-solving mindset and a
-				background in Industrial Engineering. 5 years of experience working in
-				e-commerce and tech companies, leading diverse teams and projects. I am
-				looking for a job where I can continuously learn and grow my Web
-				Development skills. Learn more {' '}
+			Software Engineer specialised in frontend and backend technologies such as
+			JavaScript ES6, React.js, Node.js, Express.js and MongoDB to build robust,
+			scalable web applications. Coming from Industrial Engineering background,
+			I bring problem-solving, adaptability, teamwork and strong time management
+			to every project. I am eager to contribute to a dynamic team where I can
+			apply my skills to solve meaningful challenges and continuously grow
+			as a developer. Learn more {' '}
 				<a
 				href='/aboutme'
 				target='_blank'
