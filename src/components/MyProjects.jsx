26982d14b8ea16345f2bf44d19b3be1db0a51081
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2025-03-07 18:34:49 +0100

    HomePage responsiveness fixed

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -2,21 +2,21 @@ import {Link} from 'react-router-dom'
 
 const MyProjects = () => {
     return (
-        <div className='flex mt-24 mb-24'>
-            <h1 className='text-3xl pl-36 mt-24'>Check out my projects</h1>
-            <div className='flex-col justify-items-end pr-56'>
+        <div className='md:flex md:mt-24 md:mb-24'>
+            <h1 className='text-3xl pl-8 md:pl-36 mt-12 md:mt-24'>Check out my projects</h1>
+            <div className='flex-col justify-items-end md:pr-56'>
 
-                <div className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff]  w-96 hover:w-[450px] mb-10 mt-8'>
+                <div className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff] ml-8 mr-8 md:w-96 hover:w-[450px] mb-10 mt-8'>
                     <Link to='/projects' state={{scrollTo: 'omercado'}}>
                         <img src='/assets/ProjectsPics/oMERCADO/homepage.png' />
                     </Link>
                 </div>
-                <div className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff]  w-96 hover:w-[450px] mb-10 mr-48'>
+                <div className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff] ml-8 mr-8 md:w-96 hover:w-[450px] mb-10 md:mr-48'>
                     <Link to='/projects' state={{scrollTo: 'tune-tracker'}}>
                         <img src='/assets/ProjectsPics/Screenshot 2024-09-17 at 18.47.32.png' />
                     </Link>
                 </div>
-                <div className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff]  w-96 hover:w-[450px] mb-8 mr-12'>
+                <div className='shadow-[-5px_-6px_5px_rgba(121,22,87,0.3),4px_10px_15px_#9966ff] ml-8 mr-8 md:w-96 hover:w-[450px] mb-8 md:mr-12'>
                     <Link to='/projects' state={{scrollTo: 'food-ninja'}}>
                         <img src='/assets/ProjectsPics/Screenshot 2024-09-17 at 18.53.22.png' />
                     </Link>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,49 +1,69 @@
 const Technologies = () => {
 	return (
-		<div className='flex mt-16 mb-32'>
-			<h1 className='text-3xl pl-36 mb-2 mt-16'>Technologies and tools</h1>
-			<ul className='pl-12 mt-24 pr-36'>
-				<li>
-					<span className='pl-32 mt-8 hover:text-xl hover:text-[#7b12ff]'>
-						CSS3
-					</span>
-					<span className='pl-32 mt-8 text-3xl hover:text-4xl hover:text-[#7b12ff]'>
-						JavaScript
-					</span>
-				</li>
-				<li className='mt-4'>
-					<span className='pl-12 hover:text-xl hover:text-[#7b12ff]'>
-						HTML5
-					</span>
-					<span className='pl-28 text-2xl hover:text-3xl hover:text-[#7b12ff]'>
-						React
-					</span>{' '}
-					<span className='pl-28 mt-6 hover:text-xl hover:text-[#7b12ff]'>
-						Jasmine
-					</span>
-				</li>
-				<li className='mt-4 '>
-					<span className='hover:text-xl hover:text-[#7b12ff]'>MongoDB</span>
-					<span className='pl-12 text-xl hover:text-2xl hover:text-[#7b12ff]'>
-						Express
-					</span>
-					<span className='pl-24 mt-4 text-2xl hover:text-3xl hover:text-[#7b12ff]'>
-						Node.js
-					</span>
-					<span className='ml-20 hover:text-xl hover:text-[#7b12ff]'>
-						Postman
-					</span>
-				</li>
-				<li className='mt-6'>
-					<span className='pl-24 hover:text-xl hover:text-[#7b12ff]'>GIT</span>
-					<span className='pl-24 text-xl hover:text-2xl hover:text-[#7b12ff]'>
-						Tailwind
-					</span>
-					<span className='ml-24 hover:text-xl hover:text-[#7b12ff]'>
-						ChakraUI
-					</span>
-				</li>
-			</ul>
+		<div>
+			<div className='md:hidden'>
+				<h1 className='text-3xl pl-8 mb-2 mt-16'>Technologies <br></br> and tools</h1>
+				<ul className='pl-8 '>
+					<li>JavaScript ES6</li>
+					<li>CSS</li>
+					<li>HTML</li>
+					<li>React.js</li>
+					<li>Node.js</li>
+					<li>Express.js</li>
+					<li>MongoDB</li>
+					<li>Tailwind</li>
+					<li>Chakra UI</li>
+					<li>Postman</li>
+					<li>Jasmine</li>
+					<li>Jira</li>
+				</ul>
+
+			</div>
+			<div className='hidden md:flex md:mt-16 md:mb-32'>
+				<h1 className='text-3xl pl-36 mb-2 mt-16'>Technologies and tools</h1>
+				<ul className='pl-12 mt-24 pr-36'>
+					<li>
+						<span className='pl-32 mt-8 hover:text-xl hover:text-[#7b12ff]'>
+							CSS3
+						</span>
+						<span className='pl-32 mt-8 text-3xl hover:text-4xl hover:text-[#7b12ff]'>
+							JavaScript ES6
+						</span>
+					</li>
+					<li className='mt-4'>
+						<span className='pl-12 hover:text-xl hover:text-[#7b12ff]'>
+							HTML5
+						</span>
+						<span className='pl-28 text-2xl hover:text-3xl hover:text-[#7b12ff]'>
+							React
+						</span>{' '}
+						<span className='pl-28 mt-6 hover:text-xl hover:text-[#7b12ff]'>
+							Jasmine
+						</span>
+					</li>
+					<li className='mt-4 '>
+						<span className='hover:text-xl hover:text-[#7b12ff]'>MongoDB</span>
+						<span className='pl-12 text-xl hover:text-2xl hover:text-[#7b12ff]'>
+							Express
+						</span>
+						<span className='pl-24 mt-4 text-2xl hover:text-3xl hover:text-[#7b12ff]'>
+							Node.js
+						</span>
+						<span className='ml-20 hover:text-xl hover:text-[#7b12ff]'>
+							Postman
+						</span>
+					</li>
+					<li className='mt-6'>
+						<span className='pl-24 hover:text-xl hover:text-[#7b12ff]'>GIT</span>
+						<span className='pl-24 text-xl hover:text-2xl hover:text-[#7b12ff]'>
+							Tailwind
+						</span>
+						<span className='ml-24 hover:text-xl hover:text-[#7b12ff]'>
+							ChakraUI
+						</span>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
